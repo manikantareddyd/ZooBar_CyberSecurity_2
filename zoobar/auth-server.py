548296edc9280ba689da5d9de0ca2c3cc4b9312a
Manikanta Reddy <manikanta.reddy.d@outlook.com>
2017-03-28 15:31:18 +0000

    Implemented auth server

--- zoobar/auth-server.py
+++ zoobar/auth-server.py
@@ -6,8 +6,18 @@ import auth
 from debug import *
 
 class AuthRpcServer(rpclib.RpcServer):
-    ## Fill in RPC methods here.
-    pass
+    def rpc_login(self, **kwargs):
+        username = kwargs['username']
+        password = kwargs['password'] 
+        return auth.login(username, password)
+    def rpc_register(self, **kwargs):
+        username = kwargs['username']
+        password = kwargs['password'] 
+        return auth.login(username, password)
+    def rpc_check_token(self, **kwargs):
+        username = kwargs['username']
+        token = kwargs['token'] 
+        return auth.login(username, token)
 
 (_, dummy_zookld_fd, sockpath) = sys.argv
 
